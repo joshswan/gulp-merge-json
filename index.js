28c6de6365b5063bbb05bd973d32ee434d9e220e
Ben Davis <bendavis78@gmail.com>
2016-01-31 20:21:33 -0600

    Allow options for JSON.stringify, optionally interpret first arg as options an object)

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ var through = require('through');
 var PLUGIN_NAME = 'gulp-merge-json';
 
 module.exports = function(fileName, edit, startObj, endObj, exportModule) {
+
+  var jsonReplacer, jsonSpace;
+  if (typeof fileName === 'object') {
+    // use first argument as opts
+    var opts = fileName;
+    fileName = opts.fileName;
+    startObj = opts.startObj;
+    endObj = opts.endObj;
+    exportModule = opts.exportModule;
+    jsonReplacer = opts.jsonReplacer || null;
+    jsonSpace = opts.jsonSpace || '\t';
+  } else {
+    jsonReplacer = null;
+    jsonSpace = '\t';
+  }
+
   if ((startObj && typeof startObj !== 'object') || (endObj && typeof endObj !== 'object')) {
     throw new gutil.PluginError(PLUGIN_NAME, PLUGIN_NAME + ': Invalid start and/or end object!');
   }
@@ -60,7 +76,7 @@ module.exports = function(fileName, edit, startObj, endObj, exportModule) {
       merged = merge(merged, endObj);
     }
 
-    var contents = JSON.stringify(merged, null, '\t');
+    var contents = JSON.stringify(merged, jsonReplacer, jsonSpace);
 
     if (exportModule === true) {
       contents = 'module.exports = ' + contents + ';';
