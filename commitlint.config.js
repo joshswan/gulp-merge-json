1e687cb9e2ea42ef4c7cb3db14a0b457dd303344
Josh Swan <joshuawswan@gmail.com>
2020-02-09 13:39:05 -0800

    chore: configure commitlint and automate releases

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*!
+ * Copyright 2015-2020 Josh Swan
+ * Released under the MIT license
+ * https://github.com/joshswan/gulp-merge/blob/master/LICENSE
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
